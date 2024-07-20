bce65145c63e918ee520793a5c20d507e5ca71ea
Lia Walsh <bew4lsh@gmail.com>
2024-07-20 18:55:52 -0400

    Created a new Python file with a dictionary to demonstrate the Rich library's table functionality.

---
+++ main.py
@@ -0,0 +1,11 @@
+# TODO: Produce an example Rich library based table based on this dictionary
+
+dict = {
+    "Field1": "Value1",
+    "Field2": "Value2",
+    "Field3": "Value3",
+    "Field4": "Value4",
+    "Field5": "Value5",
+    "Field6": "Value6",
+    "Field7": "Value7",
+}
